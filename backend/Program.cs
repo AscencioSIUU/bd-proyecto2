0cbaf7947dd8a5d4df6e0685ba3c6d896006327d
HUG0BA <127461683+BARHUG0@users.noreply.github.com>
2025-04-09 21:37:29 -0600

    Added timer
    Timer is shows after all the tasks have completed

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,19 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Npgsql;
+﻿using Npgsql;
 using System.Threading;
 
 const string CONNECTION_STRING = "Host=localhost;Port=5432;Database=proyecto_2;UserId=postgres;Password=p1aE7Eitr";
-const int TASK_AMOUNT = 20;
+const int USERS = 20;
+const int RACE_CONDITIONS_SEAT = 1;
 
-
-
-void SimulateUserReservation(string name)
+static void SimulateUserReservation(int user_id)
 {
     NpgsqlConnection connection = new NpgsqlConnection(CONNECTION_STRING);
     connection.Open();
 
     NpgsqlCommand isSeatAvailable = new NpgsqlCommand("SELECT is_available FROM seat WHERE id = @seat_id", connection);
-    isSeatAvailable.Parameters.AddWithValue("@seat_id", 1);
+    isSeatAvailable.Parameters.AddWithValue("@seat_id", RACE_CONDITIONS_SEAT);
 
     bool isAvailable = false;
     NpgsqlDataReader reader = isSeatAvailable.ExecuteReader();
@@ -39,7 +37,7 @@ void SimulateUserReservation(string name)
         string insertReservationQuery = "INSERT INTO reservation (event_id, user_id) VALUES (@event_id, @user_id) RETURNING id";
         NpgsqlCommand insertReservation = new NpgsqlCommand(insertReservationQuery, connection, transaction);
         insertReservation.Parameters.AddWithValue("@event_id", 1);
-        insertReservation.Parameters.AddWithValue("@user_id", 1);
+        insertReservation.Parameters.AddWithValue("@user_id", user_id);
 
         object reservationIdObj = insertReservation.ExecuteScalar();
         int reservationId = (int)reservationIdObj;
@@ -47,13 +45,13 @@ void SimulateUserReservation(string name)
 
         string updateSeatQuery = "UPDATE seat SET is_available = false WHERE id = @seat_id";
         NpgsqlCommand updateSeat = new NpgsqlCommand(updateSeatQuery, connection, transaction);
-        updateSeat.Parameters.AddWithValue("@seat_id", 1);
+        updateSeat.Parameters.AddWithValue("@seat_id", RACE_CONDITIONS_SEAT);
         updateSeat.ExecuteNonQuery();
         updateSeat.Dispose();
 
         string insertReservationSeatQuery = "INSERT INTO reservation_seats(seat_id, reservation_id) VALUES (@seat_id, @reservation_id)";
         NpgsqlCommand insertReservationSeat = new NpgsqlCommand(insertReservationSeatQuery, connection, transaction);
-        insertReservationSeat.Parameters.AddWithValue("@seat_id", 1);
+        insertReservationSeat.Parameters.AddWithValue("@seat_id", RACE_CONDITIONS_SEAT);
         insertReservationSeat.Parameters.AddWithValue("@reservation_id", reservationId);
         insertReservationSeat.ExecuteNonQuery();
         insertReservationSeat.Dispose();
@@ -72,10 +70,10 @@ void SimulateUserReservation(string name)
     connection.Dispose();
 }
 
-void LogSimulation(int i)
+static void LogSimulation(int i)
 {
     Console.WriteLine($"Start thread {i}");
-    SimulateUserReservation($"thread {i}");
+    SimulateUserReservation(i);
     Console.WriteLine($"End thread {i}");
 }
 
@@ -83,16 +81,27 @@ Console.WriteLine("DB concurreny simulation");
 Console.WriteLine("Press ENTER to start");
 Console.ReadLine();
 
-void RunSimulation()
+static async Task RunSimulation()
 {
-    for (int i = 0; i < TASK_AMOUNT; i++)
+    List<Task> taskList = new List<Task>();
+        
+    for (int i = 0; i < USERS; i++)
     {
         int copy = i;
-        Task.Run(() => { LogSimulation(copy); });
+        Task task = Task.Run(() => { LogSimulation(copy); });
+        taskList.Add(task);
     }
+
+    await Task.WhenAll(taskList);
 }
+var watch = System.Diagnostics.Stopwatch.StartNew();
 
-SimulateUserReservation("");
+await RunSimulation();
 
+watch.Stop();
+var elapsedMs = watch.ElapsedMilliseconds;
 
+Console.WriteLine($"{elapsedMs} ");
 Console.ReadLine();
+
+
